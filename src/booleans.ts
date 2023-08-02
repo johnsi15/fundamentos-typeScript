57da408a359ebad70a74704a7ab35ce2faf10ce8
John Serrano <andrey_201015@hotmail.com>
2023-08-02 10:32:37 -0500

    add booleans

---
+++ src/booleans.ts
@@ -0,0 +1,14 @@
+(() => {
+  let isEnable = true;
+
+  isEnable = false;
+
+  let isNew: boolean = false;
+  console.log(isNew);
+
+  const random = Math.random();
+  console.log(random);
+
+  isNew = random >= 0.5 ? true : false;
+  console.log(isNew);
+})()
